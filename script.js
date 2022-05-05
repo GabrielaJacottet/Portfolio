14df86bec0c2a755df118a612c77bb796cadc290
GabrielaJacottet <51517544+GabrielaJacottet@users.noreply.github.com>
2022-05-05 18:09:58 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+function displayAbout(){
+    let arrow = document.getElementById("animation");
+    arrow.classList.toggle("rotate");
+
+    let visible = document.getElementById("aboutme");
+
+    visible.classList.toggle("display");
+}
+
